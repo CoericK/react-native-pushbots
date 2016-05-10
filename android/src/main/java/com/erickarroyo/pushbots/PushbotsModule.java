99700122d73519cd08b9f2d1e0baf6717004d584
Layton Whiteley <layton.whiteley@gmail.com>
2016-05-09 23:45:12 -0400

    feat: add android sdk method mappings
    update
    
    update docs
    
    update docs
    
    revert build.gradle
    
    fix: android studio independent build

--- android/src/main/java/com/erickarroyo/pushbots/PushbotsModule.java
+++ android/src/main/java/com/erickarroyo/pushbots/PushbotsModule.java
@@ -1,7 +1,9 @@
 package com.erickarroyo.pushbots;
 
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 import com.pushbots.push.Pushbots;
 
 public class PushbotsModule extends ReactContextBaseJavaModule {
@@ -18,4 +20,51 @@ public class PushbotsModule extends ReactContextBaseJavaModule {
     public String getName() {
         return REACT_CLASS;
     }
+
+    @ReactMethod
+    public void regID(Promise promise){
+        try{
+            promise.resolve(Pushbots.sharedInstance().regID());
+        }catch(Exception ex){
+            promise.reject(ex);
+        }
+    }
+
+    @ReactMethod
+    public void setAlias(String alias){
+        if(alias != null && !alias.isEmpty()){
+            Pushbots.sharedInstance().setAlias(alias);
+        }
+    }
+
+    @ReactMethod
+    public void tag(String value){
+        if(value != null && !value.isEmpty()){
+            Pushbots.sharedInstance().tag(value);
+        }
+    }
+
+    @ReactMethod
+    public void untag(String value){
+        if(value != null && !value.isEmpty()){
+            Pushbots.sharedInstance().untag(value);
+        }
+    }
+
+    @ReactMethod
+    public void debug(Boolean value){
+        if(value == null){
+            value = false;
+        }
+        Pushbots.sharedInstance().debug(value);
+    }
+
+    @ReactMethod
+    public void toggleNotifications(Boolean value){
+        if(value == null){
+            value = false;
+        }
+        Pushbots.sharedInstance().setNotificationEnabled(value);
+    }
+
 }
